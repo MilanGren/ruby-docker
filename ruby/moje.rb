725f9f92c539444d4f7a15baa461600606f06e64
gre@notebook <gre@notebook>
2021-01-22 09:03:07 +0100

    ruby seems ok

--- ruby/moje.rb
+++ ruby/moje.rb
@@ -1,22 +1,20 @@
 
-#require "bundler/setup"
-#require "interpolator"
+require "bundler/setup"
+require "interpolator"
 
-#t = Interpolator::Table.new 0.1 => 2, 0.4 => 3, 0.8 => 10, 1.0 => 12, 1.2 => 11, 1.4 => 8
+t = Interpolator::Table.new 0.1 => 2, 0.4 => 3, 0.8 => 10, 1.0 => 12, 1.2 => 11, 1.4 => 8
 
 # LINEAR
-#t.style = 1
-
+t.style = 1
 
 data = "ruby script in this docker container is working OK\n\n"
 
-#[0, 0.5, 1.0, 1.5, 2.0].each {|x| data <<  t.interpolate(x) }    
+[0, 0.5, 1.0, 1.5, 2.0].each {|x| data <<  t.interpolate(x).to_s << "\n" }    
 
-
-IO::write("/myvol/output",data)
+IO::write("myvol/output",data)
 
 puts "--"
 puts "ruby script in this docker container seems working OK "
 puts "--"
 
-sleep 1
+# sleep 999
