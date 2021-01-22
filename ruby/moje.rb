af6b2f78c7ec5c022cd4ff8d09dae53d6267280e
gre@notebook <gre@notebook>
2021-01-22 09:28:02 +0100

    saving, all seems quite ok for testing

--- ruby/moje.rb
+++ ruby/moje.rb
@@ -2,16 +2,22 @@
 require "bundler/setup"
 require "interpolator"
 
+@outputPath = "myvol/output"
+
 t = Interpolator::Table.new 0.1 => 2, 0.4 => 3, 0.8 => 10, 1.0 => 12, 1.2 => 11, 1.4 => 8
 
-# LINEAR
-t.style = 1
-
-data = "ruby script in this docker container is working OK\n\n"
+data = ""
+if(File.exist?(@outputPath))
+  puts "#{@outputPath} exits"
+  data << IO::read(@outputPath) << "\n\n"
+else
+  puts "#{@outputPath} not exists"
+  data << "ruby script in this docker container is working OK\n\n"
+end
 
 [0, 0.5, 1.0, 1.5, 2.0].each {|x| data <<  t.interpolate(x).to_s << "\n" }    
 
-IO::write("myvol/output",data)
+IO::write(@outputPath,data)
 
 puts "--"
 puts "ruby script in this docker container seems working OK "
