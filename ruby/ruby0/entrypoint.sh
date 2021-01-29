06dd3737d214ab854c849642d23983ff85d398f1
gre@notebook <gre@notebook>
2021-01-29 12:17:02 +0100

    ruby0: ssh commands via conteiner IP works

--- ruby/ruby0/entrypoint.sh
+++ ruby/ruby0/entrypoint.sh
@@ -3,8 +3,15 @@
 # https://stackoverflow.com/a/20977657 !!! precist!
 
 apt-get install openssh-server --assume-yes
-/etc/init.d/ssh restart
 
+KEY=id_rsa_milan
+
+mkdir -p /root/.ssh
+cp $KEY.pub /root/.ssh/$KEY.pub
+cat $KEY.pub > /root/.ssh/authorized_keys
+chmod 0600 /root/.ssh/authorized_keys
+
+/etc/init.d/ssh restart
 
 for i in {1..9999}
 do
