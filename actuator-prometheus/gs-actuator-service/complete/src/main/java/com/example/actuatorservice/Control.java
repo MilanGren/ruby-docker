82f79d8b2a926f345044e9c975bd3e92ab7264bf
milan gren <gre@ujv.cz>
2021-02-17 14:51:02 +0100

    adding counter

--- actuator-prometheus/gs-actuator-service/complete/src/main/java/com/example/actuatorservice/Control.java
+++ actuator-prometheus/gs-actuator-service/complete/src/main/java/com/example/actuatorservice/Control.java
@@ -2,6 +2,8 @@ package com.example.actuatorservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +15,20 @@ public class Control {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+	private MeterRegistry meterRegistry;
+	private Counter mcounter ;
+
+	public Control(MeterRegistry meterRegistry) {
+		this.meterRegistry = meterRegistry;
+		mcounter = this.meterRegistry.counter("beer.orders", "type", "light"); // 1 - create a counter
+		mcounter.increment();
+		System.out.println("OK");
+	}
+
 	@GetMapping("/hello")
 	@ResponseBody
 	public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+		mcounter.increment();
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
