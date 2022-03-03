1352798cff9b40fb543fd62fdfccfbae052d7a31
gre@notebook <gre@notebook>
2022-03-03 10:07:56 +0100

    logging level

--- spring-boot-dockers-basics/src/main/java/com/example/springboot/HelloController.java
+++ spring-boot-dockers-basics/src/main/java/com/example/springboot/HelloController.java
@@ -1,13 +1,20 @@
 package com.example.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
+	// pokud nemam @slf4j, potom takto:
+	// Logger log = LoggerFactory.getLogger(HelloController.class);
+
 	@GetMapping("/")
 	public String index() {
+		log.debug("I AM A LOG");
+		System.out.println("I AM A PRINT");
 		return "Greetings from Spring Boot!";
 	}
 
